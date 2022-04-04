24024226b30645f0535124a7f957da294e82c4e3
Thomas <schmiddla@gmail.com>
2022-04-04 21:25:01 +0200

    password -> bcrypt

--- src/middleware/checkUserExists.js
+++ src/middleware/checkUserExists.js
@@ -7,7 +7,6 @@ export default async (req, res, next) => {
     const result = await userService.getUser(req.params.id);
 
     if (result && result.id === +req.params.id) {
-      console.log('result1', result);
       req.user = result;
       return next();
     } else return next(new NotFoundError('User not found'));
